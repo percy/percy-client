94b4badbc1076ab80d965e347cbcad7723c44947
nilshah98 <neelshah.1998@gmail.com>
2023-01-23 16:09:12 +0530

    [FEAT] Add support for scope parameter via scope_selector

--- lib/percy/client/snapshots.rb
+++ lib/percy/client/snapshots.rb
@@ -14,6 +14,7 @@ module Percy
             'attributes' => {
               'name' => options[:name],
               'enable-javascript' => options[:enable_javascript],
+              'scope' => options[:scope_selector],
               'minimum-height' => options[:minimum_height],
               'widths' => widths,
             },
